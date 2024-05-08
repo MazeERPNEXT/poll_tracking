6f800898db19266fc9ca709e0e01ccf2a3cd10db
jothimurugan2000 <jothimurugan2000@gmail.com>
2024-05-08 15:23:45 +0530

    api workded for mobileapp

--- polltracking/mobileapp/api/api.py
+++ polltracking/mobileapp/api/api.py
@@ -3,8 +3,9 @@ from frappe import auth
 from frappe.utils import get_site_name
 import base64
 from datetime import datetime 
+from frappe import _
 
-@frappe.whitelist( allow_guest=True )
+@frappe.whitelist(allow_guest=True )
 def mobile_login(usr,pwd):
     try:
         login_manager=frappe.auth.LoginManager()
@@ -20,82 +21,113 @@ def mobile_login(usr,pwd):
     
     api_generate=generate_keys(frappe.session.user)
     user=frappe.get_doc('User',frappe.session.user)
+    datetime_obj = datetime.strptime(user.last_login, "%Y-%m-%d %H:%M:%S.%f")
+
+    # Format the datetime object
+    user_last_login = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+    reporter = frappe.get_doc("Reporter",user.username)
     frappe.local.response['message']={
             "success_key":1,
             "message":"Authentication Success",
             "sid":frappe.session.sid,
-            "api_key":user.api_key,
-            "api_secret":api_generate['api_secret'],
+            "frappe_token":api_generate['token'],
             "email":user.email,
             "first_name":user.first_name,
             "middle_name":user.middle_name,
             "last_name":user.last_name,
             "full_name":user.full_name,
             "username":user.username,
+            "reporter":reporter,
             "role_profile_name":user.role_profile_name,
             "gender":user.gender,
-            "birth_date":user.birth_date,
+            # "birth_date":user.birth_date,
             "phone":user.phone,
             "location":user.location,
             "mobile_no":user.mobile_no,
-            "last_login":user.last_login,
+            "last_login":user_last_login,
             "banner_image":user.banner_image,
-            # "image":"https://demokegtracking.webredirect.org/"+str(user.user_image),
-            "Site_name":get_site_name(frappe.local.request.host),
-            "roles": frappe.get_roles()
+            "image":str(user.user_image),
+            "site_name":get_site_name(frappe.local.request.host),
+            "roles": frappe.get_roles(),
         }
-
+    
+    
 @frappe.whitelist( allow_guest=True )    
 def generate_keys(user):
     user_details = frappe.get_doc("User", user)
-    # api_secret = frappe.generate_hash(length=15)
+    api_secret = None
     if not user_details.api_key:
         api_key = frappe.generate_hash(length=15)
         user_details.api_key = api_key
     if not user_details.api_secret:
         api_secret = frappe.generate_hash(length=15)
-        user_details.api_secret = base64.b64encode(api_secret.encode()).decode()
-    # user_details.api_secret = api_secret
-    user_details.save()
-    return {"api_secret": base64.b64encode(user_details.api_secret.encode()).decode()}
+        user_details.api_secret = api_secret
+    else:
+        api_secret=user_details.get_password('api_secret')
+        user_details.api_secret = api_secret
+        
+    user_details.save(ignore_permissions=True)
+
+    api_key_bytes = user_details.api_key.encode('ascii')
+    api_secret_bytes = api_secret.encode('ascii')
+    base64_bytes = base64.b64encode(api_key_bytes+b':'+api_secret_bytes)
+    token = base64_bytes.decode('ascii')
+    
+    return {"token": token}
+
+# @frappe.whitelist(allow_guest=True)
+# def registered_user_profile_details(email):
+#     reporter = frappe.get_doc("Reporter",email)
+#     user = frappe.get_doc("User",reporter.user)
+#     # Convert string to datetime object
+#     datetime_obj = datetime.strptime(user.last_login, "%Y-%m-%d %H:%M:%S.%f")
+
+#     # Format the datetime object
+#     user_last_login = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+#     return {
+#             "reporter":reporter,
+#             # "user":user
+#             # "user":{
+#             #     "name":user.name,
+#             #     "first_name":user.first_name,
+#             #     "email":user.email,
+#             #     "username":user.username,
+#             #     "full_name":user.full_name,
+#             #     "language":user.language,
+#             #     "last_login":user_last_login,
+#             #     "role_profiles":user.role_profiles,
+#             #     "roles":user.roles                
+#             #      }
+#             }
+     
+@frappe.whitelist(allow_guest=True)
+def reporter_to_key_in_polliing_count(data):
+    return data
+
 
 @frappe.whitelist(allow_guest=True)
-def registered_user_profile_details(email):
-    reporter = frappe.get_doc("Reporter",email)
-    user = frappe.get_doc("User",reporter.user)
-    # Convert string to datetime object
-    datetime_obj = datetime.strptime(user.last_login, "%Y-%m-%d %H:%M:%S.%f")
+def get_constituency_and_candidates(user_email):
+    # Authenticate user
+    # user = frappe.session.user
+    user = user_email
+    if not user or not  "News Reporter" in frappe.get_roles(user):
+        frappe.throw(_("Access Denied"), frappe.PermissionError)
+    user = frappe.get_doc("User",user)
+    reporter = frappe.get_all("Reporter", filters={"user_name": user.username}, fields=["*"])
 
-    # Format the datetime object
-    user_last_login = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
-# print()
-    # Original string
-    # original_string = "2024-05-06 13:21:23.450535"
-
-    # # Convert string to datetime object
-    # datetime_obj = datetime.strptime(original_string, "%Y-%m-%d %H:%M:%S.%f")
-
-    # # Define the timezone (replace 'America/New_York' with the desired timezone)
-    # timezone = pytz.timezone('America/New_York')
-
-    # # Localize the datetime object with the timezone
-    # localized_dt = timezone.localize(datetime_obj)
-    return {
-            "reporter":reporter,
-            # "user":user
-            "user":{
-                "name":user.name,
-                "first_name":user.first_name,
-                "email":user.email,
-                "username":user.username,
-                "full_name":user.full_name,
-                "language":user.language,
-                "last_login":user_last_login,
-                "role_profiles":user.role_profiles,
-                "roles":user.roles                
-                 }
-            }
-     
+    # Retrieve reporter's constituency
+    constituency = frappe.get_value("Reporter", reporter[0].user_name, "constituency")
+    
+    # Fetch candidates and parties for the constituency
+    candidates = frappe.get_all("Election Candidate", filters={"constituency": constituency}, fields=["candidate_name", "party","party_image","votes"])
+    
+    # Prepare response
+    response = {
+        "constituency": constituency,
+        "candidates": candidates
+    }
+    return response
+# @frappe.whitelist(allow_guest=True)
 # @frappe.whitelist( allow_guest=True ) 
 # def get_sales_invoice_list():
 #     # Get all sales invoice list 
--- polltracking/poll_tracker/doctype/polling_count/polling_count.js
+++ polltracking/poll_tracker/doctype/polling_count/polling_count.js
@@ -6,79 +6,84 @@ frappe.ui.form.on("Polling Count", {
 
 	// },
     
-    // onload:function(frm){  
-    //     if (frm.is_dirty()) {
-    //         frappe.show_alert('Please save form before attaching a file')
-    //     }
-    //     frm.set_query('constituency', () => {
-    //         return {
-    //             filters: {
-    //                 constituency: ['in', []]
-    //             }
-    //         }
-    //     })
-    //     if(frm.doc.state === undefined){
-    //     frm.doc.state = "TamilNadu";
-    //     }
-    //     if(frm.doc.reporter === undefined){
-    //     frm.doc.reporter = frappe.session.user_fullname
-    //     }
-    //     if (frm.doc.constituency === undefined){
-    //         frappe.db.get_value(
-    //             "Reporter",
-    //             { user: frappe.session.user },
-    //             "constituency",
-    //             (r) => {
-    //                 if (r && r.constituency) {
-    //                     console.log(r)
-    //                     frm.set_value("constituency", r.constituency);
-    //                 }
-    //             }
-    //         );
-    //     }
+    onload:function(frm){  
+        if (frm.is_dirty()) {
+            frappe.show_alert('Please save form before attaching a file')
+        }
+        if(frappe.session.user_fullname === "Administrator"){
+            return 
+        }
+        if (frappe.sess)
+        // frm.set_query('constituency', () => {
+        //     return {
+        //         filters: {
+        //             constituency: ['in', []]
+        //         }
+        //     }
+        // })
+        if(frm.doc.state === undefined){
+        frm.doc.state = "TamilNadu";
+        }
+        if(frm.doc.reporter === undefined){
+        frm.doc.reporter = frappe.session.user_fullname
+        }
+        // if (frm.doc.constituency === undefined){
+        //     frappe.db.get_value(
+        //         "Reporter",
+        //         { user: frappe.session.user },
+        //         "constituency",
+        //         (r) => {
+        //             if (r && r.constituency) {
+        //                 console.log(r)
+        //                 frm.set_value("constituency", r.constituency);
+        //             }
+        //         }
+        //     );
+        // }
         
         
-    //     // if (!frm.is_dirty()) {
+        // if (!frm.is_dirty()) {
             
        
-    //     for (var i = 0; i < 10; i++) {
-    //         var child = cur_frm.add_child("polling_items");
-    //         frappe.model.set_value(child.doctype, child.name, "candidate", "sarathi");
-    //         frappe.model.set_value(child.doctype, child.name, "party", "DMK");
-    //         frappe.model.set_value(child.doctype, child.name, "previous_rounds_votes", "0");
-    //         frappe.model.set_value(child.doctype, child.name, "current_rounds_votes", "");
-    //         frappe.model.set_value(child.doctype, child.name, "total", "0");
+        // for (var i = 0; i < 10; i++) {
+        //     var child = cur_frm.add_child("polling_items");
+        //     frappe.model.set_value(child.doctype, child.name, "candidate", "sarathi");
+        //     frappe.model.set_value(child.doctype, child.name, "party", "DMK");
+        //     frappe.model.set_value(child.doctype, child.name, "previous_rounds_votes", "0");
+        //     frappe.model.set_value(child.doctype, child.name, "current_rounds_votes", "");
+        //     frappe.model.set_value(child.doctype, child.name, "total", "0");
 
-    //     }
-    //     var child = cur_frm.add_child("polling_items");
-    //     frappe.model.set_value(child.doctype, child.name, "party", "Total");
-    //     frappe.model.set_value(child.doctype, child.name, "previous_rounds_votes", "0");
-    //     frappe.model.set_value(child.doctype, child.name, "current_rounds_votes", "");
-    //     frappe.model.set_value(child.doctype, child.name, "total", "0");
-    //         // }
-    //     cur_frm.refresh_field("polling_items")
-    // },
+        // }
+        // var child = cur_frm.add_child("polling_items");
+        // frappe.model.set_value(child.doctype, child.name, "party", "Total");
+        // frappe.model.set_value(child.doctype, child.name, "previous_rounds_votes", "0");
+        // frappe.model.set_value(child.doctype, child.name, "current_rounds_votes", "");
+        // frappe.model.set_value(child.doctype, child.name, "total", "0");
+        //     // }
+        // cur_frm.refresh_field("polling_items")
+    },
 
-    // constituency:function(frm){
+
+    constituency:function(frm){
             
-    //     if(frm.doc.constituency){
-    //         for (var i = 0; i < 10; i++) {
-    //             var child = cur_frm.add_child("polling_items");
-    //             frappe.model.set_value(child.doctype, child.name, "candidate", "sarathi");
-    //             frappe.model.set_value(child.doctype, child.name, "party", "DMK");
-    //             frappe.model.set_value(child.doctype, child.name, "previous_rounds_votes", "0");
-    //             frappe.model.set_value(child.doctype, child.name, "current_rounds_votes", "");
-    //             frappe.model.set_value(child.doctype, child.name, "total", "0");
+        if(frm.doc.constituency){
+            for (var i = 0; i < 10; i++) {
+                var child = cur_frm.add_child("polling_items");
+                frappe.model.set_value(child.doctype, child.name, "candidate", "sarathi");
+                frappe.model.set_value(child.doctype, child.name, "party", "DMK");
+                frappe.model.set_value(child.doctype, child.name, "previous_rounds_votes", "0");
+                frappe.model.set_value(child.doctype, child.name, "current_rounds_votes", "");
+                frappe.model.set_value(child.doctype, child.name, "total", "0");
 
-    //             }
-    //             var child = cur_frm.add_child("polling_items");
-    //             frappe.model.set_value(child.doctype, child.name, "party", "Total");
-    //             frappe.model.set_value(child.doctype, child.name, "previous_rounds_votes", "0");
-    //             frappe.model.set_value(child.doctype, child.name, "current_rounds_votes", "");
-    //             frappe.model.set_value(child.doctype, child.name, "total", "0");
-    //         }
-    //         cur_frm.refresh_field("polling_items")
-    // }
+                }
+                var child = cur_frm.add_child("polling_items");
+                frappe.model.set_value(child.doctype, child.name, "party", "Total");
+                frappe.model.set_value(child.doctype, child.name, "previous_rounds_votes", "0");
+                frappe.model.set_value(child.doctype, child.name, "current_rounds_votes", "");
+                frappe.model.set_value(child.doctype, child.name, "total", "0");
+            }
+            cur_frm.refresh_field("polling_items")
+    }
 });
 
 
